99df55095ab2c49863d4ca948b8eddd93cc0424f
Colin Clark <colinbdclark@gmail.com>
2015-10-11 15:11:59 -0400

    Moves all tasks requiring user input to the beginning.

--- provisioning/scripts/provision.sh
+++ provisioning/scripts/provision.sh
@@ -4,13 +4,13 @@ sudo raspi-config
 echo "Please change the pi account password"
 ./change-password.sh
 
+echo "Configuring host"
+sudo ./set-hostname.sh $1
+sudo ./overlay-config.sh
+sudo ./disable-root.sh
+
 echo "Installing packages"
 sudo ./install-packages.sh
-
-echo "Configuring host"
-sudo ./overlay-config.sh
-sudo ./set-hostname.sh $1
-sudo ./disable-root.sh
 
 echo "Installing application"
 ./install-app.sh
